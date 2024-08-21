ad09c1e747cd61c5fa5cff01ef33a32b30627d3f
SRH-64637 <samihrahman127u2020@gmail.com>
2024-08-21 10:02:01 +0000

    Changed the story. Not yet debugged.

--- Game_algo.py
+++ Game_algo.py
@@ -1,25 +1,46 @@
 with open("story.txt", "r") as f:
     story = f.read()
-print(story)
 
+multiple_resp = {
+    "(adjective)":story.count("(adjective)"),
+    "(noun)":story.count("(noun)"),
+    "(plural nouns)":story.count("(plural nouns)"),
+    "(verb)": story.count("(verb)")
+    }
 starting_point = -1
+last_index = 0
 words = {}
+new_story = ""
+
+
+# multiple_resp stores the placeholders that occurs more than once and needs multiple distinct response
 
 for i, char in enumerate(story):
     if char == "(":
-        starting_point = i
+        starting_point = i 
 
     elif char == ")" and starting_point != -1:
-        placeholder = story[starting_point: i+1]
-        if placeholder not in words:
-            words[placeholder] = None
+        placeholder = story[starting_point: i+1].lower() # "(noun)"
+
+        if placeholder in multiple_resp.keys():
+            response = input(f"Enter a word for {placeholder} (More {multiple_resp[placeholder]} To Go): ")
+            multiple_resp[placeholder] = multiple_resp[placeholder]-1
+            new_story += story[last_index:starting_point] + response
+            last_index = i + 1
+        else:
+            if placeholder not in words:
+                words[placeholder] = None
+
         starting_point = -1
+
+# Append the remaining part of the story after the last placeholder
+new_story += story[last_index:]
 
 for key,value in words.items():
     words[key] = input(f"Enter a word for {key}: ")
-    story = story.replace(key, words[key])
+    new_story = new_story.replace(key, words[key])
 
-print(story)
+print(new_story)
 
 
         
